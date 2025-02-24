bf5caf291bc68879a83a2f44475c5fa58b5b57a9
AtchutchPlayGround <atchuth.vemula@gmail.com>
2025-02-23 21:39:28 -0600

    Initial Commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Project to test GitHub Actions.";
+    }
+
+    @GetMapping("/desc")
+    public String describe() {
+        return "This is a sample project to test GitHub Actions.";
+    }
+
+    @GetMapping("/testPath")
+    public String te() {
+        return "Sample test path.";
+    }
+}
---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloControllerTest {
+
+    @Test
+    public void testSayHello() {
+        HelloController controller = new HelloController();
+        String response = controller.sayHello();
+        assertThat(response).isEqualTo("Project to test GitHub Actions.");
+    }
+}
